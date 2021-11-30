88ef68e69b68f4ba15b210e5c7cdfde00c657946
taylor <taylor@straykitestudios.com>
2021-11-30 09:57:15 -0600

    Added placeholders

---
+++ taylor_wright/solutions/helpers.py
@@ -0,0 +1,15 @@
+from time import perf_counter
+from functools import wraps
+
+"""
+Courtesy of Chris Cunningham on the Tech-Art slack
+"""
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = perf_counter()
+        ret = func(*args, **kwargs)
+        print(f"{func.__name__.replace('_', ' ')} took: {perf_counter() - start:.8f} seconds")
+        return ret
+    return wrapper
