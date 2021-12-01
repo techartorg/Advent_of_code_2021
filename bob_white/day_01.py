ace1d090a3201d07e28e9ac217ded9b039542a7f
Bob White <robert.wd.white@gmail.com>
2021-11-30 23:12:34 -0600

    Day 01

---
+++ bob_white/day_01.py
@@ -0,0 +1,5 @@
+
+data = list(map(int, open('day_01_input.txt').read().splitlines()))
+print(sum(data[i+1] > data[i] for i in range(len(data)-1)))
+windows = [sum(data[i:i+3]) for i in range(len(data))]
+print(sum(windows[i+1] > windows[i] for i in range(len(windows)-1)))
