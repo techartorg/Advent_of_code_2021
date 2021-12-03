aaf86e79f3cd8140478a4f478a940dc03c5c73fc
Bob White <robert.wd.white@gmail.com>
2021-12-03 00:33:28 -0600

    Slightly less looping, still slow though. I should be able to speed this up by moving some of the significant bit logic out to the main loop

--- bob_white/day_03.py
+++ bob_white/day_03.py
@@ -2,7 +2,6 @@
 
 lines = [line.strip() for line in open('day_03_input.txt')]
 counts = [sum(int(c) for c in item) for item in zip(*lines)]
-gamma_str = ''.join('1' if c > len(lines)/2 else '0' for c in counts)
 gamma = int(''.join('1' if c > len(lines)/2 else '0' for c in counts),2)
 epsilon = int(''.join('1' if c < len(lines)/2 else '0' for c in counts), 2)
 print(gamma * epsilon)
@@ -20,15 +19,12 @@ def get_significant_bit(lines, idx, ox=True):
         return '0' if counts[idx] > (len(lines) // 2 ) else '1'
 
 oxygen = lines[:]
-for idx in range(len(lines[0])):
-    if len(oxygen) == 1:
-        break
-    oxygen = [line for line in oxygen if line[idx] == get_significant_bit(oxygen, idx, True)]
-print(oxygen)
 carbon = lines[:]
 for idx in range(len(lines[0])):
-    if len(carbon) == 1:
-        break
-    carbon = [line for line in carbon if line[idx] == get_significant_bit(carbon, idx, False)]
-print(carbon)
-print(int(carbon[0], 2) * int(oxygen[0], 2))
+    if len(oxygen) != 1:
+        oxygen = [line for line in oxygen if line[idx] == get_significant_bit(oxygen, idx, True)]
+    if len(carbon) != 1:
+        carbon = [line for line in carbon if line[idx] == get_significant_bit(carbon, idx, False)]
+
+
+print(int(carbon.pop(), 2) * int(oxygen.pop(), 2))
