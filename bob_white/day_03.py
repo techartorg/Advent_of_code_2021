7c74193f46c74e5ebe055daf4df323546bcfdc22
Bob White <robert.wd.white@gmail.com>
2021-12-03 00:40:46 -0600

    A bit more concise. 1) removed the int division, it was messing with my results 2) Do a not equal when checking carbon so no more special branch. Still a bit slow though, reducing the search space is what I want to optimize next.

--- bob_white/day_03.py
+++ bob_white/day_03.py
@@ -7,24 +7,17 @@ epsilon = int(''.join('1' if c < len(lines)/2 else '0' for c in counts), 2)
 print(gamma * epsilon)
 
 
-def get_significant_bit(lines, idx, ox=True):
+def get_significant_bit(lines, idx):
     counts = [sum(int(c) for c in item) for item in zip(*lines)]
-    if ox:
-        if counts[idx] == len(lines) / 2:
-            return '1'
-        return '1' if counts[idx] > (len(lines) // 2 ) else '0'
-    else:
-        if counts[idx] == len(lines) / 2:
-            return '0'
-        return '0' if counts[idx] > (len(lines) // 2 ) else '1'
+    return '1' if counts[idx] >= (len(lines) / 2 ) else '0'
 
 oxygen = lines[:]
 carbon = lines[:]
 for idx in range(len(lines[0])):
     if len(oxygen) != 1:
-        oxygen = [line for line in oxygen if line[idx] == get_significant_bit(oxygen, idx, True)]
+        oxygen = [line for line in oxygen if line[idx] == get_significant_bit(oxygen, idx)]
     if len(carbon) != 1:
-        carbon = [line for line in carbon if line[idx] == get_significant_bit(carbon, idx, False)]
+        carbon = [line for line in carbon if line[idx] != get_significant_bit(carbon, idx)]
 
 
 print(int(carbon.pop(), 2) * int(oxygen.pop(), 2))
