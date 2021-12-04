c822c5a1531fe3df27aaaf0e7a9172f1162d4a20
Bob White <robert.wd.white@gmail.com>
2021-12-04 01:36:29 -0600

    Little faster day 3

--- bob_white/day_03.py
+++ bob_white/day_03.py
@@ -11,13 +11,19 @@ def get_significant_bit(lines, idx):
     counts = [sum(int(c) for c in item) for item in zip(*lines)]
     return '1' if counts[idx] >= (len(lines) / 2 ) else '0'
 
-oxygen = lines[:]
-carbon = lines[:]
-for idx in range(len(lines[0])):
+# Find the initial oxygen vs carbon numbers, this is the only time they start from the same search space, so after this we have to reduce them in place
+oxygen = []
+carbon = []
+for line in lines:
+    if line[0] == get_significant_bit(lines, 0):
+        oxygen.append(line)
+    else:
+        carbon.append(line)
+
+for idx in range(1, len(lines[0])):
     if len(oxygen) != 1:
         oxygen = [line for line in oxygen if line[idx] == get_significant_bit(oxygen, idx)]
     if len(carbon) != 1:
         carbon = [line for line in carbon if line[idx] != get_significant_bit(carbon, idx)]
-
 
 print(int(carbon.pop(), 2) * int(oxygen.pop(), 2))
