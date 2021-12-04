2741cd5723a315dbe8bbab69334ae95ea2deda7b
Chris Cunningham <chris@rightsomegoodgames.ca>
2021-12-04 19:18:38 -0400

    day 4 removed some iterations by only checking active col and row for win

--- chris_cunningham/day_04.py
+++ chris_cunningham/day_04.py
@@ -1,32 +1,37 @@
 from math import prod
 
+Vec2 = tuple[int, int]
+
 
 class Tile(object):
-    def __init__(self, number: int):
+    def __init__(self, position: Vec2, number: int):
+        self.position: Vec2 = position
         self.number: int = number
         self.is_hit: bool = False
 
 
 class Board(object):
     def __init__(self, lines: str):
-        self.tiles: dict[tuple[int, int], Tile] = {}
+        self.tiles: dict[Vec2, Tile] = {}
         self.numbers_to_tiles: dict[int, Tile] = {}
 
         for i, row in enumerate(lines.splitlines()):
             for j, col in enumerate(row.split()):
                 position = (j, i)
                 number = int(col)
-                tile = Tile(number)
+                tile = Tile(position, number)
                 self.tiles[position] = tile
                 self.numbers_to_tiles[number] = tile
 
-    def select_number(self, n: int):
-        if n in self.numbers_to_tiles:
-            self.numbers_to_tiles[n].is_hit = True
+    def select_number(self, n: int) -> bool:
+        tile = self.numbers_to_tiles.get(n)
+        if not tile:
+            return False
 
-    def has_win(self) -> bool:
-        row_win = any(all(self.tiles[(row, i)].is_hit for i in range(5)) for row in range(5))
-        col_win = any(all(self.tiles[(i, col)].is_hit for i in range(5)) for col in range(5))
+        tile.is_hit = True
+        row, col = tile.position
+        row_win = all(self.tiles[(row, i)].is_hit for i in range(5))
+        col_win = all(self.tiles[(i, col)].is_hit for i in range(5))
         return row_win or col_win
 
     def get_score(self) -> int:
@@ -51,9 +56,7 @@ def parse_inputs(path: str) -> tuple[list[int], list[Board]]:
 def part_one(inputs: list[int], boards: list[Board]) -> int:
     for n in inputs:
         for b in boards:
-            b.select_number(n)
-
-            if b.has_win():
+            if b.select_number(n):
                 return b.get_score() * n
 
 
@@ -63,9 +66,8 @@ def part_two(inputs: list[int], boards: list[Board]) -> int:
 
     for n in inputs:
         for b in boards:
-            b.select_number(n)
-
-            if b not in closed_set and b.has_win():
+            is_win = b.select_number(n)
+            if b not in closed_set and is_win:
                 last = (n, b.get_score())
                 closed_set.add(b)
 
