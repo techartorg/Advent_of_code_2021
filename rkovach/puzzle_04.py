9fbc38f052dd653d32eb580963a01d51b0b2e382
rgkovach123 <rgkovach123@gmail.com>
2021-12-04 08:50:36 -0600

    Day 04 - Added the puzzle to the docstring, code cleanup and comments added. discovered a flaw in the solution, it worked with my input, but likley to fail on others.

--- rkovach/puzzle_04.py
+++ rkovach/puzzle_04.py
@@ -1,3 +1,102 @@
+r'''
+--- Day 4: Giant Squid ---
+You're already almost 1.5km (almost a mile) below the surface of the ocean, 
+already so deep that you can't see any sunlight. What you can see, however, 
+is a giant squid that has attached itself to the outside of your submarine.
+
+Maybe it wants to play bingo?
+
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. 
+Numbers are chosen at random, and the chosen number is marked on all boards 
+on which it appears. (Numbers may not appear on all boards.) If all numbers 
+in any row or any column of a board are marked, that board wins. (Diagonals 
+don't count.)
+
+The submarine has a bingo subsystem to help passengers (currently, you and 
+the giant squid) pass the time. It automatically generates a random order 
+in which to draw numbers and a random set of boards (your puzzle input). 
+
+For example:
+
+    7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+    22 13 17 11  0
+    8  2 23  4 24
+    21  9 14 16  7
+    6 10  3 18  5
+    1 12 20 15 19
+
+    3 15  0  2 22
+    9 18 13 17  5
+    19  8  7 25 23
+    20 11 10 24  4
+    14 21 16 12  6
+
+    14 21 17 24  4
+    10 16 15  9 19
+    18  8 23 26 20
+    22 11 13  6  5
+    2  0 12  3  7
+
+After the first five numbers are drawn (7, 4, 9, 5, and 11), there are no 
+winners, but the boards are marked as follows (shown here adjacent to each 
+other to save space):
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+    8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+    6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+    1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+After the next six numbers are drawn (17, 23, 2, 0, 14, and 21), there are 
+still no winners:
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+    8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+    6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+    1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+Finally, 24 is drawn:
+
+    22 13 17 11  0         3 15  0  2 22        14 21 17 24  4
+    8  2 23  4 24         9 18 13 17  5        10 16 15  9 19
+    21  9 14 16  7        19  8  7 25 23        18  8 23 26 20
+    6 10  3 18  5        20 11 10 24  4        22 11 13  6  5
+    1 12 20 15 19        14 21 16 12  6         2  0 12  3  7
+
+At this point, the third board wins because it has at least one complete row 
+or column of marked numbers (in this case, the entire top row is marked: 
+14 21 17 24 4).
+
+The score of the winning board can now be calculated. Start by finding the 
+sum of all unmarked numbers on that board; in this case, the sum is 188. 
+Then, multiply that sum by the number that was just called when the board 
+won, 24, to get the final score, 188 * 24 = 4512.
+
+To guarantee victory against the giant squid, figure out which board will 
+win first. What will your final score be if you choose that board?
+
+
+--- Part Two ---
+On the other hand, it might be wise to try a different strategy: let the 
+giant squid win.
+
+You aren't sure how many bingo boards a giant squid could play at once, so 
+rather than waste time counting its arms, the safe thing to do is to figure 
+out which board will win last and choose that one. That way, no matter which 
+boards it picks, it will win for sure.
+
+In the above example, the second board is the last to win, which happens 
+after 13 is eventually called and its middle column is completely marked. 
+If you were to keep playing until this point, the second board would have a 
+sum of unmarked numbers equal to 148 for a final score of 148 * 13 = 1924.
+
+Figure out which board will win last. Once it wins, what would its final 
+score be?
+
+'''
+
 import copy
 
 test_input = r'''7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
@@ -23,9 +122,12 @@ test_input = r'''7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19
 
 puzzle_input = open(__file__.replace('.py', '_input.txt')).read()
 
-
-
 def parse_input(input_):
+    """
+    The first line are the bingo numbers the call. 
+    The rest are the bingo cards separated by empty lines.
+
+    """
     lines = input_.splitlines()
     numbers = [int(x) for x in lines[0].strip().split(',')]
     boards = []
@@ -41,61 +143,31 @@ def parse_input(input_):
     return numbers, boards
 
 
-def play_bingo(numbers, boards):
-    scores = copy.deepcopy(boards)
-
-    for number in numbers:
-        for i in range(len(scores)):
-            b1 = score_board(scores[i], number)
-            if is_board_a_winner(b1):
-                return (boards[i], b1, number)
-            scores[i] = b1
-
-
-def play_bingo2(numbers, boards):
-    winningCards = []
-    winningNumbers = []
-    scores = copy.deepcopy(boards)
-    winningIndices = []
-
-    for number in numbers:
-        if len(scores) < 1:
-            break
-        for i, b in enumerate(scores):
-            if i in winningIndices:
-                continue
-            b1 = score_board(b, number)
-            if is_board_a_winner(b1):
-                winningCards.append(b1)
-                winningNumbers.append(number)
-                winningIndices.append(i)
-            else:
-                scores[i] = b1
-
-    return winningCards[-1], winningNumbers[-1]
-
-
-def part_one(input_):
-    numbers, board = parse_input(input_)
-    winningBoard, score, number = play_bingo(numbers, board)
-    finalScore = calculate_score(score, number)
-    return finalScore
-
-def part_two(input_):
-    numbers, board = parse_input(input_)
-    score, number = play_bingo2(numbers, board)
-    finalScore = calculate_score(score, number)
-    return finalScore
-
-
-def calculate_score(scores, winningNumber):
-    unmarkedSum = sum(sum(scores, []))
-    return winningNumber * unmarkedSum
+def create_board(lines):
+    """Convert the board into a list of lists.
+    
+    For example::
+        [[ 2, 77,  1, 37, 29],
+         [50,  8, 87, 12, 76],
+         [74, 88, 48, 60, 79],
+         [41, 35, 92, 33, 34],
+         [45, 52, 75, 24, 28]]
+    
+    """
+    board = []
+    for l in lines:
+        board.append([int(x) for x in l.strip().split()])
+    return board
 
 
 def score_board(board, number):
-    #columns = list(map(list, zip(*board)))
-    #print(board[0], number)
+    """
+    Check the board for the number and replace it with "Zero".
+
+    Note: This is flawed, but it worked with my test input and
+    puzzle input. Need to revisit this...
+
+    """
     for i, row in enumerate(board):
         for j, column in enumerate(row):
             if board[i][j] == number:
@@ -104,6 +176,16 @@ def score_board(board, number):
 
 
 def is_board_a_winner(board):
+    """
+    Check the rows and columns for a winner.
+    If the sum of the row or columns is zero, then all squares
+    were marked.
+
+    Note: This is flawed since "0" can be a number on the board.
+    It worked with the test input and puzzle input, but it needs
+    to be revisited.
+
+    """
     for row in board:
         if sum(row) == 0:
             return True
@@ -114,22 +196,68 @@ def is_board_a_winner(board):
     return False
 
 
-def create_board(lines):
-    board = []
-    for l in lines:
-        board.append([int(x) for x in l.strip().split()])
-    return board
+def calculate_score(scores, winningNumber):
+    """
+    Score is the sum of the un-marked squares multiplied
+    by the winning number.
+    
+    """
+    unmarkedSum = sum(sum(scores, []))
+    return winningNumber * unmarkedSum
 
-print(f'Part One: {part_one(puzzle_input)}')
-print(f'Part Two: {part_two(puzzle_input)}')
+
+def play_bingo(numbers, boards):
+    """
+    Iterate through the numbers, then iterate through the boards.
+    Once a board has won, remove it from the list and keep playing
+    with the remaining boards.
+    Each time a board wins, add it and the winning number to a list
+    so we can track the order of wins. We eventually need the first
+    board to win and the last board to win as well as their respective
+    winning numbers.
+    
+    """
+    winningCards = []
+    winningNumbers = []
+    winningIndices = []
+
+    scores = copy.deepcopy(boards)
+    for number in numbers:
+        # Stop once all the cards have won.
+        if len(scores) < 1:
+            break
+        for i, b in enumerate(scores):
+            # skip cards that already won.
+            if i in winningIndices:
+                continue
+            b1 = score_board(b, number)
+            if is_board_a_winner(b1):
+                winningCards.append(b1)
+                winningNumbers.append(number)
+                winningIndices.append(i)
+            else:
+                # update the board for the next round.
+                scores[i] = b1
+
+    return (winningCards, winningNumbers)
 
 
+def part_one(input_):
+    numbers, board = parse_input(input_)
+    score, number = play_bingo(numbers, board)
+    finalScore = calculate_score(score[0], number[0])
+    return finalScore
 
+
+def part_two(input_):
+    numbers, board = parse_input(input_)
+    score, number = play_bingo(numbers, board)
+    finalScore = calculate_score(score[-1], number[-1])
+    return finalScore
 
 
 assert part_one(test_input) == 4512
 assert part_two(test_input) == 1924
 
-
-#print(f'Part One: {}')
-#print(f'Part Two: {}')
+print(f'Part One: {part_one(puzzle_input)}')
+print(f'Part Two: {part_two(puzzle_input)}')
