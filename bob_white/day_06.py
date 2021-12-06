59a6edb3bf4b47c7ea7bdf1d92526a0b6626d49c
Bob White <robert.wd.white@gmail.com>
2021-12-05 23:28:11 -0600

    Formatting with black. Thought I had been doing this, but nope, wasn't installed.

--- bob_white/day_06.py
+++ bob_white/day_06.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
 
-fish = [int(v) for v in open('day_06_input.txt').read().split(",")]
+fish = [int(v) for v in open("day_06_input.txt").read().split(",")]
 # I should probably use the smarter solution for both parts, but at least for now I'm going to keep both the slow and fast versions.
 
 fish_one = fish[:]
 for i in range(80):
-    new = [v-1 if v != 0 else 6 for v in fish_one]
+    new = [v - 1 if v != 0 else 6 for v in fish_one]
     new.extend([8] * fish_one.count(0))
     fish_one[:] = new
 
 print(len(fish_one))
-
+# Basically just grouping the fish instead of tracking each individually
 fish_age_counter: defaultdict[int, int] = defaultdict(int)
 for f in fish:
     fish_age_counter[f] += 1
@@ -19,7 +19,7 @@ for i in range(256):
     new_ages: defaultdict[int, int] = defaultdict(int)
     for age, count in fish_age_counter.items():
         if age:
-            new_ages[age-1] += count
+            new_ages[age - 1] += count
         else:
             new_ages[6] += count
             new_ages[8] += count
