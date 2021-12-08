40c2f42dd0ba286356cf66cdef78f9af73a83a66
rgkovach123 <rgkovach123@gmail.com>
2021-12-08 09:36:18 -0600

    day 08 - docstring cleanup

--- rkovach/puzzle_08.py
+++ rkovach/puzzle_08.py
@@ -1,9 +1,16 @@
 f'''--- Day 8: Seven Segment Search ---
-You barely reach the safety of the cave when the whale smashes into the cave mouth, collapsing it. Sensors indicate another exit to this cave at a much greater depth, so you have no choice but to press on.
 
-As your submarine slowly makes its way through the cave system, you notice that the four-digit seven-segment displays in your submarine are malfunctioning; they must have been damaged during the escape. You'll be in a lot of trouble without them, so you'd better figure out what's wrong.
+You barely reach the safety of the cave when the whale smashes into the cave 
+mouth, collapsing it. Sensors indicate another exit to this cave at a much 
+greater depth, so you have no choice but to press on.
 
-Each digit of a seven-segment display is rendered by turning on or off any of seven segments named a through g:
+As your submarine slowly makes its way through the cave system, you notice 
+that the four-digit seven-segment displays in your submarine are malfunctioning; 
+they must have been damaged during the escape. You'll be in a lot of trouble 
+without them, so you'd better figure out what's wrong.
+
+Each digit of a seven-segment display is rendered by turning on or off any of 
+seven segments named a through g:
 
       0:      1:      2:      3:      4:
      aaaa    ....    aaaa    aaaa    ....
@@ -99,12 +106,14 @@ segments (highlighted above).
 
 In the output values, how many times do digits 1, 4, 7, or 8 appear?
 
+
 --- Part Two ---
 Through a little deduction, you should now be able to determine the remaining 
 digits. Consider again the first example above:
 
-acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
-cdfeb fcadb cdfeb cdbaf
+    acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
+    cdfeb fcadb cdfeb cdbaf
+
 After some careful analysis, the mapping between signal wires and segments only 
 make sense in the following configuration:
 
@@ -118,37 +127,40 @@ make sense in the following configuration:
 
 So, the unique signal patterns would correspond to the following digits:
 
-acedgfb: 8
-cdfbe: 5
-gcdfa: 2
-fbcad: 3
-dab: 7
-cefabd: 9
-cdfgeb: 6
-eafb: 4
-cagedb: 0
-ab: 1
+    - acedgfb: 8
+    - cdfbe: 5
+    - gcdfa: 2
+    - fbcad: 3
+    - dab: 7
+    - cefabd: 9
+    - cdfgeb: 6
+    - eafb: 4
+    - cagedb: 0
+    - ab: 1
+
 Then, the four digits of the output value can be decoded:
 
-cdfeb: 5
-fcadb: 3
-cdfeb: 5
-cdbaf: 3
+    - cdfeb: 5
+    - fcadb: 3
+    - cdfeb: 5
+    - cdbaf: 3
+
 Therefore, the output value for this entry is 5353.
 
 Following this same process for each entry in the second, larger example above, 
 the output value of each entry can be determined:
 
-fdgacbe cefdb cefbgd gcbe: 8394
-fcgedb cgb dgebacf gc: 9781
-cg cg fdcagb cbg: 1197
-efabcd cedba gadfec cb: 9361
-gecf egdcabf bgf bfgea: 4873
-gebdcfa ecba ca fadegcb: 8418
-cefg dcbef fcge gbcadfe: 4548
-ed bcgafe cdgba cbgef: 1625
-gbdfcae bgc cg cgb: 8717
-fgae cfgab fg bagce: 4315
+    - fdgacbe cefdb cefbgd gcbe: 8394
+    - fcgedb cgb dgebacf gc: 9781
+    - cg cg fdcagb cbg: 1197
+    - efabcd cedba gadfec cb: 9361
+    - gecf egdcabf bgf bfgea: 4873
+    - gebdcfa ecba ca fadegcb: 8418
+    - cefg dcbef fcge gbcadfe: 4548
+    - ed bcgafe cdgba cbgef: 1625
+    - gbdfcae bgc cg cgb: 8717
+    - fgae cfgab fg bagce: 4315
+
 Adding all of the output values in this larger example produces 61229.
 
 For each entry, determine all of the wire/segment connections and decode the 
