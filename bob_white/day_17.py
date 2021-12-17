bdfc9f695609f5d2a1b22733e42c469c334b11bb
Bob White <robert.wd.white@gmail.com>
2021-12-16 23:42:09 -0600

    Optimized Day 17 Turns out calling `min` in the while loop check is expensive (and dumb because I already had that variable) Tweaked the names to be a bit more than 2 characters.

--- bob_white/day_17.py
+++ bob_white/day_17.py
@@ -1,20 +1,18 @@
-import time
-
 example = "x=20..30, y=-10..-5"
 example = "x=85..145, y=-163..-108"
 # Parse the puzzle input into some ranges so I can do containment checks
-xr, yr = example.split(", ")
-xr, yr = xr[2:], yr[2:]
-xn, xm = map(int, xr.split(".."))
-yn, ym = map(int, yr.split(".."))
-xr = set(range(xn, xm + 1))
-yr = set(range(yn, ym + 1))
+_x_range, _y_range = example.split(", ")
+_x_range, _y_range = _x_range[2:], _y_range[2:]
+x_min, x_max = map(int, _x_range.split(".."))
+y_min, y_max = map(int, _y_range.split(".."))
+x_range = set(range(x_min, x_max + 1))
+y_range = set(range(y_min, y_max + 1))
 
 
 def hits_range(dx: int, dy: int) -> tuple[bool, int]:
     x, y = (0, 0)
     max_height = 0
-    while y > min(yr):
+    while y > y_min and x <= x_max:
         x += dx
         y += dy
         if dx < 0:
@@ -22,17 +20,17 @@ def hits_range(dx: int, dy: int) -> tuple[bool, int]:
         elif dx > 0:
             dx -= 1
         dy -= 1
-        max_height = max(y, max_height)
-        if x in xr and y in yr:
+        if y > max_height:
+            max_height = y
+        if x in x_range and y in y_range:
             return True, max_height
     return False, -1
 
 
-start = time.time()
 valid: list[int] = []
-for idx in range(max(xr) + 1):
-    for jdx in range(min(yr), abs(min(yr))):
-        hit, m_height = hits_range(idx, jdx)
+for x in range(x_max + 1):
+    for y in range(y_min, abs(y_min)):
+        hit, m_height = hits_range(x, y)
         if hit:
             valid.append(m_height)
-print(max(valid), len(valid), time.time() - start)
+print(max(valid), len(valid))
